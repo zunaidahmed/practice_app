19503fcbda1cafe3e45673224d4c69c7cb548289
Zunaid Ahmed <zunaidahmmed@gmail.com>
2024-01-19 16:55:43 +0600

    TechStore Practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,117 @@
+import 'package:code_practice_with_flutter/dashboard_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const TechStore());
 }
 
-class MyApp extends StatelessWidget {
+class TechStore extends StatelessWidget {
+  const TechStore({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-
-
-
- /* int _selectedIndex = 0;*/
-
-  /*List<Widget> screens = [
-    const Text('HOME'),
-    const Text('PROFILE'),
-    const Text('SEARCH'),
-    const Text('SETTING'),
-  ];
-*/
-
-  List<Widget> list = [
-
-    const Text('HOME'),
-    const Text('PERSON'),
-    const Text('SEARCH'),
-    const Text('SETTING'),
-    const Text('MAN'),
-
-  ];
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 5, child: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
-        title: const Text('Facebook'),
-        bottom: const TabBar(
-          indicatorColor: Colors.white,
-          unselectedLabelColor: Colors.red,
-          labelColor: Colors.blue,
-          tabs: [
-            Tab(
-              icon: Icon(Icons.home),
-              text: 'HOME',
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: _formkey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (String?text ) {
+
+                      if(text!.isEmpty){
+
+                        return 'Please Email';
+
+                      }
+
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (String? text){
+
+                      if(text!.isEmpty){
+
+                        return 'fill up';
+                      }
+
+                    },
+
+
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+
+                      onPressed: () {
+                        if(_formkey.currentState!.validate()){
+
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>DashboardScreen()));
+
+                        }
+
+
+                      },
+
+
+
+                      child: const Text('Next'),
+                    ),
+                  ),
+                ],
+              ),
             ),
-
-            Tab(
-              icon: Icon(Icons.person),
-              text: 'PERSON',
-            ),
-
-            Tab(
-              icon: Icon(Icons.search),
-              text: 'SEARCH',
-            ),
-
-            Tab(
-              icon: Icon(Icons.settings),
-              text: 'SETTING',
-            ),
-
-            Tab(
-              icon: Icon(Icons.face),
-              text: 'FACE',
-            ),
-          ],
-          
-        ),
-
-      ),
-
-      body: const TabBarView(
-        children: [
-              Center(child: Text('HOME')),
-              Center(child: Text('PERSON')),
-              Center(child: Text('SEARCH')),
-              Center(child: Text('SETTING')),
-              Center(child: Text('FACE')),
-            ],
           ),
-
+        ),
       ),
-
     );
-
   }
 }
