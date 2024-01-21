49ac668d5d5531770bcc4ce8723985c262a4ef95
Zunaid Ahmed <zunaidahmmed@gmail.com>
2024-01-21 20:14:19 +0600

    Sum App Practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:code_practice_with_flutter/dashboard_screen.dart';
+
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(const TechStore());
@@ -12,106 +13,9 @@ class TechStore extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: ();
     );
   }
 }
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Form(
-              key: _formkey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Login',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (String?text ) {
-
-                      if(text!.isEmpty){
-
-                        return 'Please Email';
-
-                      }
-
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (String? text){
-
-                      if(text!.isEmpty){
-
-                        return 'fill up';
-                      }
-
-                    },
-
-
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-
-                      onPressed: () {
-                        if(_formkey.currentState!.validate()){
-
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=>DashboardScreen()));
-
-                        }
-
-
-                      },
-
-
-
-                      child: const Text('Next'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
