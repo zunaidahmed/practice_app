34d1bde7c2e3dde5da095acc6bc6b42066283d64
Zunaid Ahmed <zunaidahmmed@gmail.com>
2024-02-12 20:01:48 +0600

    Assignment of Module 14

---
+++ lib/photo_details.dart
@@ -0,0 +1,32 @@
+import 'package:code_practice_with_flutter/photo_gallery.dart';
+import 'package:flutter/material.dart';
+
+class PhotoDetails extends StatelessWidget {
+  final PhotoGallery photo;
+
+  const PhotoDetails({Key? key, required this.photo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Photo Details'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(photo.url ?? ''),
+              SizedBox(height: 15),
+              Text('Title: ${photo.title ?? ''}', style: TextStyle(fontSize: 20),),
+              SizedBox(height: 5),
+              Text('ID: ${photo.id ?? ''}', style: TextStyle( fontSize: 20),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/photo_list_screen.dart
+++ lib/photo_list_screen.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
+
+import 'package:code_practice_with_flutter/photo_details.dart';
 import 'package:code_practice_with_flutter/photo_gallery.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart';
-
 
 class PhotoListScreen extends StatefulWidget {
   const PhotoListScreen({super.key});
@@ -12,15 +14,14 @@ class PhotoListScreen extends StatefulWidget {
 }
 
 class _PhotoListScreenState extends State<PhotoListScreen> {
-
   List<PhotoGallery> galleryList = [];
+  bool _getPhotoGalleryProgress = false;
 
   @override
   void initState() {
     super.initState();
-   getPhotoGalleryList();
+    getPhotoGalleryList();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -28,32 +29,49 @@ class _PhotoListScreenState extends State<PhotoListScreen> {
       appBar: AppBar(
         title: const Text('Photo Gallery App'),
       ),
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return const ListTile(
-              leading: CircleAvatar(
-                backgroundImage:
-                    NetworkImage('https://rabbil.com/files/rabbil.jpg'),
-              ),
-              title: Text('ABCD'),
-            );
-          }),
+      body: Visibility(
+        visible: _getPhotoGalleryProgress == false,
+        replacement: const Center(child: CircularProgressIndicator()),
+        child: ListView.builder(
+            itemCount: galleryList.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage:
+                      NetworkImage(galleryList[index].thumbnailUrl ?? ''),
+                ),
+                title: Text(galleryList[index].title ?? ' '),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>PhotoDetails(photo: galleryList[index])));
+                },
+              );
+            }),
+      ),
     );
-
   }
 
   Future<void> getPhotoGalleryList() async {
-
+    _getPhotoGalleryProgress = true;
+    setState(() {});
     Uri uri = Uri.parse('https://jsonplaceholder.typicode.com/photos');
     Response response = await get(uri);
 
-    print(response);
-    print(response.statusCode);
-    print(response.body);
+    if (response.statusCode == 200) {
+      var decodePhoto = jsonDecode(response.body);
 
+      for (var item in decodePhoto) {
+        PhotoGallery photoGallery = PhotoGallery(
+            albumId: item['albumId'],
+            id: item['id'],
+            title: item['title'],
+            url: item['url'],
+            thumbnailUrl: item['thumbnailUrl']);
 
+        galleryList.add(photoGallery);
+      }
+    }
 
-
+    _getPhotoGalleryProgress = false;
+    setState(() {});
   }
 }
